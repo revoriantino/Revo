dc651995a1765e3897c20ae5277d570fde1e9a1f
revo riantino <revoriantino@gmail.com>
2021-10-29 16:18:30 +0700

    DATA

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -9,22 +9,18 @@
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
               <i class="icon-layout menu-icon"></i>
-              <span class="menu-title">UI Elements</span>
-              <i class="menu-arrow"></i>
+              <span class="menu-title">PENGAJUAN SKRIPSI</span>
+              
             </a>
             <div class="collapse" id="ui-basic">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
-              </ul>
+              
             </div>
           </li>
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
               <i class="icon-columns menu-icon"></i>
-              <span class="menu-title">Form elements</span>
-              <i class="menu-arrow"></i>
+              <span class="menu-title">KERJA PRAKTEK</span>
+              
             </a>
             <div class="collapse" id="form-elements">
               <ul class="nav flex-column sub-menu">
