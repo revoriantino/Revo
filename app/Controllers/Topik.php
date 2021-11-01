e3b968aacce79c44a1a04c258631d9a3f59c3054
revo riantino <revoriantino@gmail.com>
2021-11-01 17:29:29 +0700

    data4

--- app/Controllers/Topik.php
+++ app/Controllers/Topik.php
@@ -1,5 +1,5 @@
 <?php
-
+ 
 namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
---
+++ app/Libraries/Rest.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Liberies;
+
+class Rest 
+{
+    public function callRest($url)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+        CURLOPT_URL => "http://10.10.20.67//api/v1/$url",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_POSTFIELDS => ""
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+        return json_decode($response);
+        }
+    }
+}
